e91062a9c392326273d2964a229e1fd6cc0d6f42
douglas <douglasmaxton@gmail.com>
2023-09-22 12:29:00 +0100

    Added the img element with attributes to be added to the div

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,19 @@
 // global variables
 let numberOfTries = 10;
+
+function hangmanImages() {
+  let images = `assets/images/hangman/${numberOfTries}.png`;
+  let id = document.getElementById("hangman-images");
+
+  let img = document.createElement("img");
+  img.setAttribute("src", images);
+  img.setAttribute("alt", "Images for hangman incorrect answers");
+  img.setAttribute("width", "200px");
+  img.setAttribute("height", "200px");
+
+  id.appendChild(img);
+
+  id.value;
+}
+
+hangmanImages();
