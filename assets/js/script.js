9fe8a137d02bcca9b882d42e8c03db66091de7f8
douglas <douglasmaxton@gmail.com>
2023-09-22 12:57:24 +0100

    Added constructive comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ function hangmanImages() {
   let images = `assets/images/hangman/${numberOfTries}.png`;
   let id = document.getElementById("hangman-images");
 
+  // Creating and setting attributes for the image
   let img = document.createElement("img");
   img.setAttribute("id", "hanging");
   img.setAttribute("src", images);
@@ -12,8 +13,10 @@ function hangmanImages() {
   img.setAttribute("width", "150rem");
   img.setAttribute("height", "auto");
 
+  // add image element to div
   id.appendChild(img);
 
+  // insert all created elements to div
   id.value;
 }
 
