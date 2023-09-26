47ebbb49b297a78e82ee90a562f02ded5a961c2f
douglas <douglasmaxton@gmail.com>
2023-09-26 15:36:12 +0100

    created a for of loop to print out the spacing from array of letters

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,20 @@ let timer;
 document.addEventListener("DOMContentLoaded", function () {
   const { name } = randomArrayValue(); // using object destructuring will get the name out of the object in names.js
   let splitNameToArray = name.split(""); // once an object has been grabbed will split the letters to an array
+  let arrayBlanks = []; // array is responsible for the blank lines and will be updated based on keydown letters
+
+  // for of loop to create the spacing for letters in an array
+  for (let i of splitNameToArray) {
+    if (i !== " ") {
+      arrayBlanks.push("_");
+    } else {
+      arrayBlanks.push(" ");
+    }
+  }
 
   // Functions to load on page start
   hangmanImages(); // dynamically loads the hangman images to the display
-  displayArrayToScreen(splitNameToArray); // function to load letters on display
+  displayArrayToScreen(arrayBlanks); // function to load letters on display
 
   document.addEventListener("keydown", function (event) {
     const alphabet = "abcdefghijklmnopqrstuvwxyz";
