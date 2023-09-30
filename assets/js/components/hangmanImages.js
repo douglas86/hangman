0180faed315945e3c4147e7663a26085e7a12733
douglas <douglasmaxton@gmail.com>
2023-09-30 22:34:17 +0100

    created a ternary operator for image

--- assets/js/components/hangmanImages.js
+++ assets/js/components/hangmanImages.js
@@ -4,7 +4,10 @@
 export const hangmanImages = () => {
   let triesLeft = Number(document.getElementById("tries").innerHTML);
 
-  let images = `assets/images/hangman/${triesLeft}.png`;
+  let images =
+    triesLeft > 0
+      ? `assets/images/hangman/${triesLeft}.png`
+      : `assets/images/hangman/0.png`;
   let id = document.getElementById("hanging");
 
   id.setAttribute("src", images);
