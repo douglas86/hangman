2dd194fe9acf28a03098f85053ffa05274863fad
douglas <douglasmaxton@gmail.com>
2023-09-30 22:25:30 +0100

    renamed variable tries to triesLeft

--- assets/js/components/hangmanImages.js
+++ assets/js/components/hangmanImages.js
@@ -2,9 +2,9 @@
  * generate the src attribute dynamically
  */
 export const hangmanImages = () => {
-  let tries = Number(document.getElementById("tries").innerHTML);
+  let triesLeft = Number(document.getElementById("tries").innerHTML);
 
-  let images = `assets/images/hangman/${tries}.png`;
+  let images = `assets/images/hangman/${triesLeft}.png`;
   let id = document.getElementById("hanging");
 
   id.setAttribute("src", images);
