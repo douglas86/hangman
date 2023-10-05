b3308e7de6850baf37963bcf811cc30e7d29205d
douglas <douglasmaxton@gmail.com>
2023-10-05 13:54:33 +0100

    created docstring for button.js

--- assets/js/components/button.js
+++ assets/js/components/button.js
@@ -3,6 +3,11 @@ import { displayLetter } from "./displayLetter.js";
 import { createArray } from "../utils/createArray.js";
 import { findLetterInArray } from "../utils/findLetterInArray.js";
 
+/**
+ * creates the keyboard buttons on the screen
+ * @param objectAttributes - passing an object of all the attributes that I am wanting to create
+ * @param splitNameToArray - this is only passed in to update the other functions that it needs to run
+ */
 export const button = (objectAttributes, splitNameToArray) => {
   let div = document.getElementsByClassName("keyboard-layout")[0];
   let btn = document.createElement("button");
@@ -11,10 +16,12 @@ export const button = (objectAttributes, splitNameToArray) => {
     key === "text"
       ? (btn.innerText = `${value}`) &&
         btn.addEventListener("click", function () {
+          // sets the button attribute to change class so that different styling can be applied
           btn.setAttribute("class", "keyboard-buttons-clicked");
-          createArray(splitNameToArray, value.toLowerCase());
-          findLetterInArray(splitNameToArray, value.toLowerCase());
-          displayLetter(value);
+
+          createArray(splitNameToArray, value.toLowerCase()); // updates array on display
+          findLetterInArray(splitNameToArray, value.toLowerCase()); // finds button in array and updates scores accordingly
+          displayLetter(value); // displays a capital letter to the screen on button click
         })
       : btn.setAttribute(key, `${value}`);
   });
