07cf037c3ecd95146db0966d8ac68ef6ef2fdc06
douglas <douglasmaxton@gmail.com>
2023-10-05 11:32:25 +0100

    changed importing an audio file 2 an absolute path

--- assets/js/components/decreaseScore.js
+++ assets/js/components/decreaseScore.js
@@ -6,7 +6,7 @@ import { hangmanImage } from "./hangmanImage.js";
 export const decreaseScore = () => {
   let scoring = document.getElementById("scoring");
   let triesLeft = document.getElementById("tries");
-  let audio = new Audio("../../sounds/pencil.mp3");
+  let audio = new Audio("assets/sounds/pencil.mp3");
 
   scoring.textContent = (Number(scoring.textContent) - 1).toString();
   triesLeft.textContent = (Number(triesLeft.textContent) - 1).toString();
