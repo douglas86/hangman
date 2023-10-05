cd6e889527b4facd2561dcaa91d2d2a6d090254e
douglas <douglasmaxton@gmail.com>
2023-10-05 10:45:53 +0100

    used an absolute path to call audio file

--- assets/js/components/increaseScore.js
+++ assets/js/components/increaseScore.js
@@ -1,10 +1,9 @@
 import { playAudio } from "../utils/playAudio.js";
-import file from "../../sounds/tada.mp3";
 
 export const increaseScore = () => {
   let id = document.getElementById("scoring");
 
   id.textContent = (Number(id.textContent) + 2).toString();
 
-  playAudio(file);
+  playAudio("../../assets/sounds/tada.mp3");
 };
