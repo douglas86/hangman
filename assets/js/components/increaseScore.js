a7db7d93d41eeb0979c026433f23e3249afbd7c5
douglas <douglasmaxton@gmail.com>
2023-10-05 10:43:25 +0100

    fix: import file rather than just calling it

--- assets/js/components/increaseScore.js
+++ assets/js/components/increaseScore.js
@@ -1,9 +1,10 @@
 import { playAudio } from "../utils/playAudio.js";
+import file from "../../sounds/tada.mp3";
 
 export const increaseScore = () => {
   let id = document.getElementById("scoring");
 
   id.textContent = (Number(id.textContent) + 2).toString();
 
-  playAudio("assets/sounds/tada.mp3");
+  playAudio(file);
 };
