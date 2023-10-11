bd3a3e91c3eaf1e765235aba05dbdce3e78cda33
douglas <douglasmaxton@gmail.com>
2023-10-11 20:34:49 +0100

    reset word on correct characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ import { qwertyKeyboard } from "./components/qwertyKeyboard.js";
 import { checkLetterUsed } from "./utils/checkLetterUsed.js";
 import { overlay } from "./components/overlay.js";
 import { randomValue } from "./utils/randomValue.js";
+import { splitStringToArray } from "./utils/splitStringToArray.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
@@ -10,12 +11,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
   let sound = document.getElementsByClassName("sound")[0];
   let play = document.getElementsByClassName("play-sound")[0];
-  let data = document.getElementById("hidden-data");
-  let splitNameToArray = data.innerText.split("");
+  let hidden = document.getElementById("hidden-data");
+  let guess = document.getElementsByClassName("guessed-word")[0];
+  let keyboard = document.getElementsByClassName("keyboard-buttons-clicked");
   let keysPressed = []; // captures all the keys that were pressed
 
-  createArray(splitNameToArray);
-  qwertyKeyboard(splitNameToArray);
+  createArray(splitStringToArray());
+  qwertyKeyboard(splitStringToArray());
   overlay();
 
   // This event listener attaches to the music button
@@ -26,6 +28,25 @@ document.addEventListener("DOMContentLoaded", function () {
       : (play.innerText = "OFF");
   });
 
+  const config = { childList: true };
+
+  const callback = (mutationList) => {
+    for (const mutation of mutationList) {
+      if (mutation.type === "childList") {
+        guess.innerHTML = "";
+        createArray(splitStringToArray());
+        // when word changes reset all buttons back to original class
+        Object.values(keyboard).map((item) => {
+          item.setAttribute("class", "keyboard-buttons");
+        });
+      }
+    }
+  };
+
+  const observer = new MutationObserver(callback);
+
+  observer.observe(hidden, config);
+
   document.addEventListener("keydown", function (event) {
     let keys = document.getElementsByClassName("keyboard-buttons");
     if (alphabet.includes(event.key.toLowerCase())) {
@@ -33,12 +54,10 @@ document.addEventListener("DOMContentLoaded", function () {
       // if a letter has not been pressed update scores
       // if a letter has been pressed do nothing
       keysPressed.indexOf(event.key.toLowerCase()) === -1 &&
-        checkLetterUsed(splitNameToArray, event.key);
+        checkLetterUsed(splitStringToArray(), event.key);
 
       // when a letter on keyboard used append to keysPressed array
       keysPressed.push(event.key.toLowerCase());
-
-      // !result[0].split("").includes("_") && randomValue();
 
       // when a letter is pressed on the keyboard, the class of the keyboard will change
       // to give different styling to keys
---
+++ assets/js/utils/splitStringToArray.js
@@ -0,0 +1,3 @@
+export const splitStringToArray = () => {
+  return document.getElementById("hidden-data").innerText.split("");
+};
