9f9a6c1a722624679fc239a74d8695305806a844
douglas <douglasmaxton@gmail.com>
2023-10-12 14:32:40 +0100

    added all docstrings to functions

--- assets/js/utils/line-break.js
+++ assets/js/utils/line-break.js
@@ -1,5 +1,9 @@
 import { button } from "../components/button.js";
 
+/**
+ * This creates the line break tag, giving a qwerty keyboard layout onscreen
+ * @param objectAttributes
+ */
 export const lineBreak = (objectAttributes) => {
   let div = document.getElementsByClassName("keyboard-layout")[0];
   let b = document.createElement("br");
--- assets/js/utils/split-string-to-array.js
+++ assets/js/utils/split-string-to-array.js
@@ -1,3 +1,7 @@
+/**
+ * this will split the hidden word from the web page into an array to be used throughout
+ * @returns {string[]}
+ */
 export const splitStringToArray = () => {
   return document.getElementById("hidden-data").innerText.split("");
 };
