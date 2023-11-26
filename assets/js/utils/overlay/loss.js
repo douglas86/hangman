fe31dcb31ef3642c592abd8fe452c87a088fa2b3
douglas <douglasmaxton@gmail.com>
2023-11-26 16:31:57 +0000

    change flag value to false with loss overlay

--- assets/js/utils/overlay/loss.js
+++ assets/js/utils/overlay/loss.js
@@ -5,6 +5,7 @@ export const loss = () => {
   let id = document.getElementById("overlay");
   let text = document.getElementsByClassName("overlay-text")[0];
   let hidden = document.getElementById("hidden-data").innerText;
+  let flag = document.getElementsByClassName("hidden")[0]; // Gets hidden flag value from index.html
 
   id.style.display = "block";
 
@@ -15,6 +16,8 @@ export const loss = () => {
         <p>Would you like to play again?</p>
         <button class="overlay-button" onclick="window.location.reload()">Reset</button>
     `;
+
+  flag.innerHTML = `False`;
 
   id.addEventListener("click", function () {
     id.style.display = "none";
