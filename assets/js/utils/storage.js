63208bf1df3e5820505b6252221211aee9d346e8
douglas <douglasmaxton@gmail.com>
2023-11-27 18:43:16 +0000

    added descriptive comments to storage file

--- assets/js/utils/storage.js
+++ assets/js/utils/storage.js
@@ -1,21 +1,26 @@
 import { checkLetterUsed } from "./check-letter-used.js";
 import { splitStringToArray } from "./split-string-to-array.js";
 
+// keeps track of letters that were pressed
 let keysPressed = [];
 
-/*
-Stores array in localStorage only if a letter was not used
+/**
+ * Stores array in localStorage only if a letter was not used
  */
 export const storage = (letter) => {
+  // gets array from localStorage
   const parsing = JSON.parse(localStorage.getItem("keysPressed"));
 
   parsing !== null
-    ? parsing.indexOf(letter.toLowerCase()) === -1 &&
+    ? // if array found in localStorage
+      parsing.indexOf(letter.toLowerCase()) === -1 &&
       keysPressed.push(letter.toLowerCase()) &&
       checkLetterUsed(splitStringToArray(), letter)
-    : keysPressed.push(letter.toLowerCase()) &&
+    : // if no array found in localStorage
+      keysPressed.push(letter.toLowerCase()) &&
       checkLetterUsed(splitStringToArray(), letter);
 
+  // store keysPressed array to localStorage
   let string = JSON.stringify(keysPressed);
   localStorage.setItem("keysPressed", string);
 };
